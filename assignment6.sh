378d2c87c9cb796a9a3bfab49ded19f42b7454bd
Davin Lee <dlee27@nd.edu>
2020-09-21 17:22:46 -0400

    i forgot to add the append part but here it is now

--- assignment6.sh
+++ assignment6.sh
@@ -1,5 +1,5 @@
 #task 1
-cat wages.csv | tail -n 3294 | cut -d , -f 1,2 | tr ',' ' ' | sort -u -V
+cat wages.csv | tail -n 3294 | cut -d , -f 1,2 | tr ',' ' ' | sort -u -V >> gender-yearsexperience.txt
 
 #task 2
 cat wages.csv | sort -t, -k4,4 -nr
