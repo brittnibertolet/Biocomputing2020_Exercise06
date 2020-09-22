d2524c78f0370efff7987c6839ad1082507996f8
c-evans-7 <cevans7@nd.edu>
2020-09-22 14:53:07 -0400

    Create script and Complte task #1 in Exercise 6

---
+++ Ex6.sh
@@ -0,0 +1,12 @@
+# Shell script which does the following:
+# 1.) writes a file containing the unique gener-yearsExperience combinations in the file "wages.csv"
+# 2.) Returns the following to stdout upon execution:
+#    - The gender, yearsExperience, and wage for the highest earner
+#    - The gender, yearsExperience, and wage for the lowest earner
+#    - The number of females in the top ten earners in this dataset
+
+# Task #1:
+cat wages.csv | cut -d, -f 1,2 | sort -t, -k1,1 -k2,2n | tr "," " " | uniq > gender_Exp_Sorted.txt 
+
+# Task #2:
+
