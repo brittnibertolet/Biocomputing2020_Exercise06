c715379ee7702f5bccbdf660171de40746f2dcd8
Emma Aleksandrovic <ealeksa2@nd.edu>
2020-09-23 20:21:13 -0400

    Emma's attempt at challenge 2

--- HighestLowest_wage.sh
+++ HighestLowest_wage.sh
@@ -1,17 +1,20 @@
 # Returns to sd out: gender, yearsExpe- rience, and wage for the highest earner and lowest earner, number of females in top 10
 
+#Lowest wage
 for file in "wages.csv"
 do
 cat "$file" | sort -t, -k4,4 -n | cut -f 1,2,4 -d, | head -n2
 done
 
+# Highest wage
 for file in "wages.csv"
 do
 cat "$file" | sort -t, -k4,4 -n | cut -f 1,2,4 -d, | tail -n1
 done
 
+#Number of females in top 10 earners
 for file in "wages.csv"
 do
-cat "$file" | sort -t, -k4,4 -n | head -n10 | grep -o "female" | wc -l  
+cat "$file" | sort -t, -k4,4 -n | head -n11 | grep -o "female" | wc -l  
 done
 
