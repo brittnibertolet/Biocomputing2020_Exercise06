e25efcf2c7fab2af1189fa57c0ada526539f64e8
Kyle Walker <kwalke11@nd.edu>
2020-09-24 19:49:21 -0400

    updated usage

--- wages2.sh
+++ wages2.sh
@@ -1,8 +1,8 @@
 #Returns top earner with years of experience and wage, bottom earner with years of experience and wage, and number of females in top 10 earners.
-# usage bash wages2.sh file_name
+# usage bash wages2.sh
 echo -n "top earner: " 
-cat "$1" | cut -d , -f 1,2,4 | sort -k 4 -r | head -n 1 
+cat wages.csv | cut -d , -f 1,2,4 | sort -k 4 -r | head -n 1 
 echo -n "bottom earner: " 
-cat "$1" | cut -d , -f 1,2,4 | sort -k 4 -r | tail -n 1
+cat wages.csv | cut -d , -f 1,2,4 | sort -k 4 -r | tail -n 1
 echo -n "number of females in top 10:"
-cat "$1" | cut -d , -f 1,2,4 | sort -k 4 -r | head -n 10 | grep female | wc -l 
+cat wages.csv | cut -d , -f 1,2,4 | sort -k 4 -r | head -n 10 | grep female | wc -l 
