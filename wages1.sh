dccad6e3d85418cc607c664f40ed35a1f1c1887f
Jordan Brown <jbrown36@nd.edu>
2020-09-24 18:40:39 -0400

    fixed usage

--- wages1.sh
+++ wages1.sh
@@ -1,7 +1,7 @@
 #Sorts wages.csv  alphabetically in first column and numerically in second column
-#usage: bash wages1.sh filename
+#usage: bash wages1.sh
 
-cat wages.csv | cut -d , -f 1,2 | sort -t , -k1,1 -k2n,2 | uniq |sed '/gender/d' |  tr ',' ' ' >> unique1.txt
+cat wages.csv | cut -d , -f 1,2 | sort -t , -k1,1 -k2n,2 | uniq | sed '/gender/d' |  tr ',' ' ' >> unique1.txt
 
 
 
