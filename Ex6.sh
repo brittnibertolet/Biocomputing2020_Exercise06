3eac6409c3d7a36297e2518bd0655cce564793fd
DanMarshalla <dmarsha7@nd.edu>
2020-09-24 19:22:25 -0400

    Part 2 of exercise 6 added

--- Ex6.sh
+++ Ex6.sh
@@ -9,4 +9,9 @@
 cat wages.csv | cut -d, -f 1,2 | sort -t, -k1,1 -k2,2n | tr "," " " | uniq > gender_Exp_Sorted.txt 
 
 # Task #2:
-
+echo "Highest earner"
+cat wages.csv | cut -d, -f 1,2,4 | sort -t, -n -k3 | tail -n 1
+echo "Lowest earner"
+cat wages.csv | cut -d, -f 1,2,4 | sort -t, -n -k3 | head -n 2 | tail -n 1
+echo "Top ten female earners"
+cat wages.csv | cut -d, -f 1,2,4 | sort -t, -n -k3 -r | head -n 10 | grep -c "female"
