c5135e7cc6f8ab7915a378bee2e8464357266551
Kyle Walker <kwalke11@nd.edu>
2020-09-24 17:46:54 -0400

    updated #2

--- wages2.sh
+++ wages2.sh
@@ -1,1 +1,7 @@
 # usage bash wages2.sh
+echo -n "top earner: " 
+cat "$1" | cut -d , -f 1,2,4 | sort -k 4 -r | head -n 1 
+echo -n "bottom earner: " 
+cat "$1" | cut -d , -f 1,2,4 | sort -k 4 -r | tail -n 1
+echo -n "number of females in top 10:"
+cat "$1" | cut -d , -f 1,2,4 | sort -k 4 -r | head -n 10 | grep female | wc -l 
