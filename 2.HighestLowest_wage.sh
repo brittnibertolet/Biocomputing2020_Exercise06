fce4aed524a714f932a9f8406994522079918902
Emma Aleksandrovic <ealeksa2@nd.edu>
2020-09-24 16:55:31 -0400

    challenge 2 for submitting

---
+++ 2.HighestLowest_wage.sh
@@ -0,0 +1,22 @@
+# Returns to sd out: gender, yearsExpe- rience, and wage for the 
+# highest earner and lowest earner, number of females in top 10
+# note, for head command check if -n1 gives you column name 
+
+#Lowest wage
+for file in "wages.csv"
+do
+cat "$file" | sort -t, -k4,4 -n | cut -f 1,2,4 -d, | head -n2
+done
+
+# Highest wage
+for file in "wages.csv"
+do
+cat "$file" | sort -t, -k4,4 -n | cut -f 1,2,4 -d, | tail -n1
+done
+
+#Number of females in top 10 earners
+for file in "wages.csv"
+do
+cat "$file" | sort -t, -k4,4 -n | head -n11 | grep -o "female" | wc -l  
+done
+
