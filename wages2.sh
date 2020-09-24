31f1bc2a23b5c51f4ba7ad62b8479f7aedd988bc
Kyle Walker <kwalke11@nd.edu>
2020-09-24 17:49:56 -0400

    updated part 2

--- wages2.sh
+++ wages2.sh
@@ -1,3 +1,4 @@
+#Returns top earner with years of experience and wage, bottom earner with years of experience and wage, and number of females in top 10 earners.
 # usage bash wages2.sh
 echo -n "top earner: " 
 cat "$1" | cut -d , -f 1,2,4 | sort -k 4 -r | head -n 1 
